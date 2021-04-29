dcfd2653a19066152d5ea11bf794b2ecaab262bc
moooosh <73801599+moooosh@users.noreply.github.com>
2021-04-29 18:24:36 +1000

    Added Footer Information

--- src/App.js
+++ src/App.js
@@ -10,6 +10,17 @@ function App() {
     <div className="App">
     <Weather />
 
+    <strong>
+      {" "}
+      Code by Shanese Mattiske |{" "}
+      <a href="/" target="_blank">
+        Netlify App
+      </a>{" "}
+      |{" "}
+      <a href="https://github.com/moooosh/weather-react" target="_blank">
+        GitHub
+      </a>
+    </strong>
     </div>
   );
 }
