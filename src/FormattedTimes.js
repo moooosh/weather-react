32cfc350508d43f20651197250aaf8e15e82b445
moooosh <73801599+moooosh@users.noreply.github.com>
2021-05-01 16:19:15 +1000

    Fixed Time

--- src/FormattedTimes.js
+++ src/FormattedTimes.js
@@ -2,13 +2,13 @@ import React from "react";
 
 
 export default function FormattedTimes(props) {
-   
-    let hours = props.date.getHours();
+
+    let hours = props.time.getHours();
     if (hours < 10) {
       hours = `0${hours}`;
     }
 
-    let minutes = props.date.getMinutes();
+    let minutes = props.time.getMinutes();
     if (minutes < 10) {
       minutes = `0${minutes}`;
     }
