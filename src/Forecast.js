7167073ad55e19618462882692c765f021ee1b68
moooosh <73801599+moooosh@users.noreply.github.com>
2021-05-08 16:45:33 +1000

    Added useEffect for City Search bug

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ForecastDaily from "./ForecastDaily.js";
 import "./Forecast.css"
@@ -12,6 +12,9 @@ export default function Forecast(props) {
         setResult(true);
     }
 
+    useEffect(() => {
+        setResult(false);
+    }, [props.coordinates]);
         
       
 if (result) {
